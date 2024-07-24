85c482cac672406afb03bb8e99a39f202181fc71
rchavan <rchavan@greenlighthealth.com>
2024-07-24 19:28:28 +0530

    spring-security

--- src/main/java/com/auth/AuthServiceApplication.java
+++ src/main/java/com/auth/AuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
